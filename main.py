0760e7e49d1cac5820c350a925b8a8bb1bf84246
Kokila-chandrakar <payalchandrakar327@gmail.com>
2026-05-17 23:30:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+from urllib.parse import urlparse
